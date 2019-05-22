a7c3d5348c2dcfc817bb9bfb08db30c20e805e06
hdore <hdore@ucsb.edu>
2019-05-22 11:06:09 -0700

    binder needed files

---
+++ install.R
@@ -0,0 +1,4 @@
+pkgs = c("reshape2", "ggplot2", "vegan", "plyr", "data.table", "tidyverse", "tibble", "jcolors", "RColorBrewer", "forcats")
+ncores = parallel::detectCores()
+install.packages(pkgs, Ncpus = ncores)
+
